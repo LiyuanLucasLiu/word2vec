a9e7bf274c8623c3b7fd36ff97c4e956d6e96ac8
LiyuanLucasLiu <llychinalz@gmail.com>
2017-01-22 13:53:49 -0600

    training thread done

--- src/note.h
+++ src/note.h
@@ -1,39 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <pthread.h>
 
 #define MAX_STRING 100
 #define EXP_TABLE_SIZE 1000
 #define MAX_EXP 6
 #define MAX_SENTENCE_LENGTH 1000
 #define MAX_CODE_LENGTH 40
-
-const int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
+#define FREE(x) if (x != NULL) {free(x);}
+#define CHECKNULL(x) if (x == NULL) {printf("Memory allocation failed\n"); exit(1);}
 
 typedef float real;                    // Precision of float numbers
 
-struct vocab_word {
-  long long cn;
-  int *point;
-  char *word, *code, codelen;
+struct supervision {
+  long long function_id;
+  long long label;
+};
+
+struct training_ins {
+  long long id;
+  long long c_num;
+  long long *cList;
+  long long sup_num;
+  struct supervision *supList;
 };
 
 char train_file[MAX_STRING], output_file[MAX_STRING];
 char save_vocab_file[MAX_STRING], read_vocab_file[MAX_STRING];
-struct vocab_word *vocab;
-int binary = 0, debug_mode = 2, window = 5, min_count = 5, num_threads = 1, min_reduce = 1;
-int *vocab_hash;
-long long vocab_max_size = 1000, vocab_size = 0, layer1_size = 100;
-long long train_words = 0, word_count_actual = 0, file_size = 0, classes = 0;
+// struct vocab_word *vocab;
+long long  *cCount;
+int binary = 0, debug_mode = 2, reSample = 10, min_count = 5, num_threads = 1, min_reduce = 1;
+long long c_size = 0, c_length = 100, l_size = 1, l_length = 1000, d_size;
+long long ins_num = 191965, ins_count_actual = 0;
+long long iters = 10;
 real alpha = 0.025, starting_alpha, sample = 0;
-real *syn0, *syn1, *syn1neg, *expTable;
+
+struct training_ins * data;
+real *c, *l, *d, *cneg, *db, *lb;
+real *o;
+real *expTable;
 clock_t start;
 
-int negative = 0;
+int negative = 5;
 const int table_size = 1e8;
-int *table;
+long long *table;
 
 void InitUnigramTable()
 void ReadWord(char *word, FILE *fin)
